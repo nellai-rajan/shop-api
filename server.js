2b94a456f898b84695e0dc942e344acbc36ad3b5
Nellairajan C <nellairajan.c@credopay.com>
2026-09-06 21:26:26 +0530

    update:db url connect fixes

--- server.js
+++ server.js
@@ -17,11 +17,23 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/order', require('./routes/orderRoutes'));
 
 
-// DB connect
-mongoose.connect(process.env.DB_URL)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+// // DB connect
+// mongoose.connect(process.env.DB_URL)
+// .then(() => console.log('MongoDB Connected'))
+// .catch(err => console.log(err));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Server running on port", PORT);
+    });
+  })
+  .catch(err => console.log(err));
